ca3a09acc09ec898986126c8d5785497bfe9b1e2
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2025-01-08 14:32:43 +0100

    CMake full config and tests base config

--- dag_graph_validator/test/test.cc
+++ dag_graph_validator/test/test.cc
@@ -2,10 +2,16 @@
 #include <cstdlib>
 #include <cstring>
 #include <array>
+#include <iostream>
 
 #include "gtest/gtest.h"
 
 
 TEST(ExampleTest, Example) {
     ASSERT_EQ(1, 1);
+}
+
+int main() {
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
 }
