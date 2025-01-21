b77a9460299a6c16723f7181ff41c695fef41bfa
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2025-01-21 11:50:44 +0100

    Update csv reader header file

---
+++ cap/src/io/csv_reader.hpp
@@ -0,0 +1,28 @@
+/*!
+    \file csv_reader.hpp
+    \brief CSV reader functions definition
+    \authors CHARLES Rémy, CARON Maxime
+*/
+
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+#include "graph.hpp"
+
+/*!
+    \brief Check if a file exists
+    \param filename The name of the file to check
+    \return True if the file exists, false otherwise
+*/
+bool checkFileExists(const char *filename);
+
+/*!
+    \brief Read a CSV file and fill the graph with the data
+    \param graph the graph to add the edge to
+    \param file the file to read
+    \return A boolean indicating if the graph was well filled
+*/
+void loadDataset(Graph* graph, const char* filename);
